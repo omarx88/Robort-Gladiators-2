96a71977a514fa8af3a0d435fbcbdbc7809837be
Prince of Zamunda <omarxsawyer@gmail.com>
2021-11-02 11:24:05 -0600

    fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,22 @@
-window.alert("this is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+//Note the lack of quotation marks around playerName
+//window.alert(playerName);
+
+// What is this?
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log (10+10);
+// What is this?
+console.log("Our robot's name is " + playerName);
+
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+//fight(); 
+
+
